447055c56df244b74ee8675eea1f04b3496a984c
fosseddy <markfosseddy@gmail.com>
2023-05-04 19:35:58 +0500

    set up timer

--- public/main.js
+++ public/main.js
@@ -1,25 +1,91 @@
+const timer = new Worker("/timer.js");
+
 const value = document.querySelector("#value");
-const button = document.querySelector("button");
+const control = document.querySelector("#control");
+const reset = document.querySelector("#reset");
 
 const alarm = new Audio("/alarm.ogg");
 alarm.loop = true;
 
-const timer = new Worker("/timer.js");
+const TimerState = {
+    Paused: "paused",
+    Active: "active",
+    Finished: "finished"
+};
 
-timer.addEventListener("message", event => {
-    switch (event.data.type) {
-        case "change-value":
-            value.textContent = event.data.payload;
-            break;
-        case "play-alarm":
-            alarm.play();
-            break;
-        default:
-            console.log("Main: unknown event type", event.data.type);
-            break;
+let timerState = TimerState.Paused;
+
+const duration = 3 * 1000;
+
+control.addEventListener("click", () => {
+    debugger;
+    switch (timerState) {
+    case TimerState.Paused:
+        //timer.postMessage({ type: "start" });
+        control.textContent = "stop";
+        timerState = TimerState.Active;
+        break;
+    case TimerState.Active:
+        //timer.postMessage({ type: "stop" });
+        control.textContent = "start";
+        timerState = TimerState.Paused;
+        break;
+    case TimerState.Finished:
+        //if (alarm.paused) return;
+
+        //alarm.pause();
+        //alarm.currentTime = 0;
+        control.textContent = "ok";
+        break;
+    default:
+        console.error("unreachable", timerState);
+        break;
     }
 });
 
-button.addEventListener("click", () => {
-    timer.postMessage({ type: "start" });
+reset.addEventListener("click", () => {
+    timer.postMessage({ type: "reset" });
+    renderTime(value, 0);
 });
+
+timer.addEventListener("message", event => {
+    console.assert(event.data.type === "tick");
+
+    const elapsed = event.data.payload;
+
+    if (elapsed >= 1000) {
+        renderTime(value, elapsed - elapsed % 1000);
+    }
+
+    if (elapsed >= duration) {
+        alarm.play().catch(console.error);
+    }
+});
+
+function parseTime(secs) {
+    return [
+        Math.floor(secs / 3600),
+        Math.floor(secs / 60),
+        Math.floor(secs % 60)
+    ];
+}
+
+function renderTime(container, elap) {
+    const [h, m, s] = parseTime((duration - elap) * 0.001);
+    let value = "";
+
+    if (h > 0) {
+        value += `${h}h `;
+    }
+
+    if (m > 0) {
+        value += `${m}m `;
+    }
+
+    value += `${s}s`;
+
+    container.textContent = value;
+}
+
+renderTime(value, 0);
+timer.postMessage({ type: "set-duration", payload: duration });
--- public/timer.js
+++ public/timer.js
@@ -1,58 +1,53 @@
-const duration = 3 * 1000;
-let elapsed = 0;
+let duration;
+let prevTimestamp;
 
-let prevTimestamp = null;
+let frame = null;
+let elapsed = 0;
 
 function update(timestamp) {
     if (!prevTimestamp) {
         prevTimestamp = timestamp;
     }
 
-    const dt = timestamp - prevTimestamp;
+    elapsed += timestamp - prevTimestamp;
     prevTimestamp = timestamp;
 
-    elapsed += dt;
+    if (elapsed > duration) elapsed = duration;
 
-    const a = Math.max(duration - elapsed, 0) * 0.001;
-    const [h, m, s] = parseTime(a);
+    self.postMessage({ type: "tick", payload: elapsed });
 
-    let value = "";
-
-    if (h > 0) {
-        value += `${h}h `;
+    if (elapsed < duration) {
+        frame = self.requestAnimationFrame(update);
     }
-
-    if (m > 0) {
-        value += `${m}m `;
-    }
-
-    value += `${s}s`;
-
-    self.postMessage({ type: "change-value", payload: value });
-
-    if (elapsed > duration) {
-        self.postMessage({ type: "play-alarm" });
-        return;
-    }
-
-    self.requestAnimationFrame(update);
-}
-
-function parseTime(secs) {
-    return [
-        Math.floor(secs / 3600),
-        Math.floor(secs / 60),
-        Math.floor(secs % 60)
-    ];
 }
 
 self.addEventListener("message", event => {
     switch (event.data.type) {
-        case "start":
-            self.requestAnimationFrame(update);
-            break;
-        default:
-            console.log("Worker: unknown event type", event.data.type);
-            break;
+    case "start":
+        if (!frame) {
+            frame = self.requestAnimationFrame(update);
+        }
+        break;
+    case "stop":
+        if (frame) {
+            self.cancelAnimationFrame(frame);
+            frame = null;
+            prevTimestamp = null;
+        }
+        break;
+    case "reset":
+        if (frame) {
+            self.cancelAnimationFrame(frame);
+            frame = null;
+        }
+        prevTimestamp = null;
+        elapsed = 0;
+        break;
+    case "set-duration":
+        duration = event.data.payload;
+        break;
+    default:
+        console.error("Worker: unknown event type", event.data.type);
+        break;
     }
 });
